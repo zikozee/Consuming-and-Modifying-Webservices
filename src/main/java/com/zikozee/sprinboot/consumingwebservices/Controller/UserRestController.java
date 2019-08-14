0a37d37587c8c0298da4f6864f7d9baf5d479049
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2019-08-14 21:31:50 +0100

    custom exception/ Global exception handling

--- src/main/java/com/zikozee/sprinboot/consumingwebservices/Controller/UserRestController.java
+++ src/main/java/com/zikozee/sprinboot/consumingwebservices/Controller/UserRestController.java
@@ -1,18 +1,19 @@
 package com.zikozee.sprinboot.consumingwebservices.Controller;
 
 import com.zikozee.sprinboot.consumingwebservices.Service.UserService;
+import com.zikozee.sprinboot.consumingwebservices.globalHandling.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Usercontroller {
+public class UserRestController {
 
     private UserService userService;
 
     @Autowired
-    public Usercontroller(UserService theuserService){
+    public UserRestController(UserService theuserService){
         userService = theuserService;
     }
 
@@ -24,7 +25,9 @@ public class Usercontroller {
 
     @GetMapping("/user/{userId}")
     public String getUser(@PathVariable int userId){
-
+        if(userId >= userService.userList().size() || userId<= 0){
+            throw new UserNotFoundException("Student id not found -  " + userId);
+        }
         return "User==>" + userService.user(userId);
     }
 
@@ -35,7 +38,11 @@ public class Usercontroller {
 
     @GetMapping("/address/{userId}")
     public String getUserAddress(@PathVariable int userId){
-
+        if(userId >= userService.userList().size() || userId<= 0){
+            throw new UserNotFoundException("Student id not found -  " + userId);
+        }
         return "User==>" + userService.userAddress(userId);
     }
+
+    //You Can Extend As you wish
 }
